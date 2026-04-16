420fb71845981498999d8537407ec1a2541340d5
7amdyabuelkhair <hamdyabuelkhair@gmail.com>
2026-04-16 18:03:58 +0200

    Add Supabase anon key

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -3,5 +3,5 @@
 // NEVER put the service_role key here.
 
 window.SUPABASE_URL = "https://jnefierjhiyutfhwguqw.supabase.co";
-window.SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpuZWZpZXJqaGl5dXRmaHdndXF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNDY1MzUsImV4cCI6MjA5MTkyMjUzNX0.m0OJOImKKCsVLgaBRVqTJK0zISTWpUVyb5F49F7myBg";
 
